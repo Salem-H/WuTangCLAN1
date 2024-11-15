17c5fcfcca7552e4e5ae5d23bc24136ce5a5118f
Diamond Layweh <diamondlayweh@gmail.com>
2024-11-15 16:06:16 -0500

    link

---
+++ Fixso/data.js
@@ -0,0 +1,14 @@
+folders = [
+    "HTML",
+    "CSS",
+    "JS"
+];
+
+links = [
+    {
+        folder: 0,
+        name: "Code Academy",
+        desc: "coding",
+        href: "https://www.codecademy.com/courses/introduction-to-javascript"
+    }
+]
