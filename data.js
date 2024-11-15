eaea1812f3b62e7d3e7b7532f283846cdfe3160e
Salem H <salemkassal@gmail.com>
2024-11-15 13:23:45 -0500

    update

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ folders = [
 links = [
     {
         folder: 0,
-        name: "MDN",
+        name: "W3SCHOOL",
         desc: "Monzilla",
         href: "https://www.w3schools.com/html/default.asp#gsc.tab=0"
     }
