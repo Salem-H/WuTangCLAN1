dc078d3eb4c0a6ea8e83298d9584c0c986e78120
Salem H <salemkassal@gmail.com>
2024-11-15 13:23:09 -0500

    data.js update

---
+++ data.js
@@ -0,0 +1,14 @@
+folders = [
+    "HTML",
+    "CSS",
+    "JS"
+];
+
+links = [
+    {
+        folder: 0,
+        name: "MDN",
+        desc: "Monzilla",
+        href: "https://www.w3schools.com/html/default.asp#gsc.tab=0"
+    }
+]
